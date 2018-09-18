339600012f5021263889e49551a14678b54980c3
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2018-09-18 17:36:38 +0300

    simplifies

--- index.php
+++ index.php
@@ -93,13 +93,11 @@ $lots = [
             <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и
                 горнолыжное снаряжение.</p>
             <ul class="promo__list">
-                <?php foreach ($categories as $category) {
-                    print "<li class=\"promo__item promo__item--boards\">
-                    <a class=\"promo__link\" href=\"pages/all-lots.html\">$category</a>
-                </li>";
-                }
-                ?>
-
+                <?php foreach ($categories as $category): ?>
+                    <li class="promo__item promo__item--boards">
+                        <a class="promo__link" href="pages/all-lots.html"><?= $category ?></a>
+                    </li>
+                <?php endforeach; ?>
             </ul>
         </section>
         <section class="lots">
@@ -139,13 +137,11 @@ $lots = [
 <footer class="main-footer">
     <nav class="nav">
         <ul class="nav__list container">
-            <?php foreach ($categories as $category) {
-                print "<li class=\"nav__item\">
-                    <a href=\"pages/all-lots.html\">$category</a>
-                </li>";
-            }
-
-            ?>
+            <?php foreach ($categories as $category): ?>
+                <li class="nav__item">
+                    <a href="pages/all-lots.html"><?= $category ?></a>
+                </li>
+            <?php endforeach; ?>
         </ul>
     </nav>
     <div class="main-footer__bottom container">
