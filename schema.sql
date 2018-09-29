6e3b1186620c37e2ceebaa65086d933d837f4bdc
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2018-09-29 18:03:48 +0300

    fix issues

--- schema.sql
+++ schema.sql
@@ -1,5 +1,7 @@
 # use mysql.server start/stop — to start/stop mysql server
 
+drop database yeticave;
+
 CREATE DATABASE IF NOT EXISTS yeticave
   DEFAULT CHAR SET utf8
   DEFAULT COLLATE utf8_general_ci;
@@ -16,22 +18,18 @@ CREATE TABLE users (
   avatar_url VARCHAR(100)
 );
 
-EXPLAIN users;
-
-DROP TABLE users;
-
 CREATE TABLE categories (
-  id   INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  id   INT UNSIGNED PRIMARY KEY,
   name VARCHAR(80) NOT NULL UNIQUE
 );
 
-INSERT INTO categories (name)
-VALUES ('Доски и лыжи'),
-       ('Крепления'),
-       ('Ботинки'),
-       ('Одежда'),
-       ('Инструменты'),
-       ('Разное');
+INSERT INTO categories (id, name)
+VALUES (0, 'Доски и лыжи'),
+       (1, 'Крепления'),
+       (2, 'Ботинки'),
+       (3, 'Одежда'),
+       (4, 'Инструменты'),
+       (5, 'Разное');
 
 CREATE TABLE lots
 (
@@ -44,15 +42,15 @@ CREATE TABLE lots
   closes_at   TIMESTAMP    NOT NULL,
   bid_step    INT, # Bid step. What it used for?
 
-  author      INT UNSIGNED NOT NULL,
-  winner      INT UNSIGNED,
-  category    INT UNSIGNED NOT NULL,
+  author_id      INT UNSIGNED NOT NULL,
+  winner_id      INT UNSIGNED,
+  category_id    INT UNSIGNED NOT NULL,
 
-  FOREIGN KEY (author) REFERENCES users (id)
+  FOREIGN KEY (author_id) REFERENCES users (id)
     ON DELETE CASCADE,
-  FOREIGN KEY (winner) REFERENCES users (id)
+  FOREIGN KEY (winner_id) REFERENCES users (id)
     ON DELETE CASCADE,
-  FOREIGN KEY (category) REFERENCES categories (id)
+  FOREIGN KEY (category_id) REFERENCES categories (id)
     ON DELETE RESTRICT
 );
 
@@ -61,9 +59,9 @@ CREATE TABLE bids (
   id         INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   created_at TIMESTAMP,
 
-  owner      INT UNSIGNED NOT NULL,
-  lot        INT UNSIGNED NOT NULL,
+  owner_id      INT UNSIGNED NOT NULL,
+  lot_id        INT UNSIGNED NOT NULL,
 
-  FOREIGN KEY (owner) REFERENCES users (id),
-  FOREIGN KEY (lot) REFERENCES lots (id)
+  FOREIGN KEY (owner_id) REFERENCES users (id),
+  FOREIGN KEY (lot_id) REFERENCES lots (id)
 );
