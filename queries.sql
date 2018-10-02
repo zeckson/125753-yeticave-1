372af0a023633d6c974e35094796db0d926d2e0e
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2018-10-02 16:20:15 +0300

    add bid_step fix queries optimize interval arithmetic

--- queries.sql
+++ queries.sql
@@ -1,4 +1,16 @@
-# clean all categories
+# clean all
+# bids
+DELETE
+FROM bids;
+
+#lots
+DELETE FROM lots;
+
+#users
+DELETE
+FROM users;
+
+#categories
 DELETE
 FROM categories;
 
@@ -10,9 +22,6 @@ VALUES (0, 'Доски и лыжи'),
        (3, 'Одежда'),
        (4, 'Инструменты'),
        (5, 'Разное');
-
-DELETE
-FROM users;
 
 # setup test users
 INSERT INTO users (name, email, password)
@@ -32,17 +41,19 @@ WHERE email = 'vitaly@bandit.com' INTO @vitka;
 SELECT @evgesha, @vitka;
 
 # create some lots
-DELETE
-FROM lots;
-
 # open lots
-INSERT INTO lots (name, category_id, start_price, image_url, author_id)
-VALUES ('2014 Rossignol District Snowboard', 0, 10999, 'img/lot-1.jpg', @evgesha),
-       ('DC Ply Mens 2016/2017 Snowboard', 0, 15999, 'img/lot-2.jpg', @vitka),
-       ('Крепления Union Contact Pro 2015 года размер L/XL', 1, 8000, 'img/lot-3.jpg', @evgesha),
-       ('Ботинки для сноуборда DC Mutiny Charocal', 2, 10999, 'img/lot-4.jpg', @vitka),
-       ('Куртка для сноуборда DC Mutiny Charocal', 3, 7500, 'img/lot-5.jpg', @evgesha),
-       ('Маска Oakley Canopy', 5, 5400, 'img/lot-§.jpg', @vitka);
+INSERT INTO lots (name, category_id, start_price, image_url, closed_at, author_id)
+VALUES ('2014 Rossignol District Snowboard', 0, 10999, 'img/lot-1.jpg', NOW() + INTERVAL 1 WEEK, @evgesha),
+       ('DC Ply Mens 2016/2017 Snowboard', 0, 15999, 'img/lot-2.jpg', NOW() + INTERVAL 1 WEEK, @vitka),
+       ('Крепления Union Contact Pro 2015 года размер L/XL',
+        1,
+        8000,
+        'img/lot-3.jpg',
+        NOW() + INTERVAL 1 WEEK,
+        @evgesha),
+       ('Ботинки для сноуборда DC Mutiny Charocal', 2, 10999, 'img/lot-4.jpg', NOW() + INTERVAL 1 WEEK, @vitka),
+       ('Куртка для сноуборда DC Mutiny Charocal', 3, 7500, 'img/lot-5.jpg', NOW() + INTERVAL 1 WEEK, @evgesha),
+       ('Маска Oakley Canopy', 5, 5400, 'img/lot-§.jpg', NOW() + INTERVAL 1 WEEK, @vitka);
 
 # closed lots
 INSERT INTO lots (name, category_id, start_price, bid_step, created_at, closed_at, image_url, author_id, winner_id)
@@ -50,8 +61,8 @@ VALUES ('Сноуборд BF snowboards Scoop (17-18)',
         0,
         6690,
         100,
-        TIMESTAMPADD(WEEK, -1, NOW()),
-        NOW(),
+        NOW() - INTERVAL 1 WEEK,
+        NOW() + INTERVAL 1 MONTH,
         'img/lot-1.jpg',
         @vitka,
         @evgesha),
@@ -59,8 +70,8 @@ VALUES ('Сноуборд BF snowboards Scoop (17-18)',
         3,
         8999,
         200,
-        TIMESTAMPADD(MONTH, -1, NOW()),
-        TIMESTAMPADD(WEEK, -1, NOW()),
+        NOW(),
+        NOW() + INTERVAL 1 WEEK,
         'img/lot-5.jpg',
         @evgesha,
         @vitka),
@@ -68,14 +79,11 @@ VALUES ('Сноуборд BF snowboards Scoop (17-18)',
         4,
         2030,
         50,
-        TIMESTAMPADD(MONTH, -2, NOW()),
-        TIMESTAMPADD(MONTH, -1, NOW()),
+        NOW() - INTERVAL 2 MONTH,
+        NOW() + INTERVAL 2 WEEK,
         'https://www.kant.ru/upload/iblock/4df/4dfe47ce30dcf18550ba678ed54e5332.JPG',
         @evgesha,
         @vitka);
-
-DELETE
-FROM bids;
 
 # create a few bids
 INSERT INTO bids (lot_id, amount, owner_id)
@@ -96,22 +104,23 @@ FROM categories;
 # get all latest open lots. Select only name, start_price, image_url, price, number of bids, category name
 SELECT lot.id,
        lot.name,
+       lot.bid_step,
        start_price,
        IFNULL(MAX(bid.amount), start_price) AS price,
        image_url,
        category.name                        AS category_name,
        count(bid.id)                        AS bids_count
 FROM lots lot
-       LEFT JOIN categories category ON lot.category_id = category.id
-       LEFT JOIN bids bid ON lot.id = bid.lot_id
-WHERE lot.closed_at IS NULL
+            LEFT JOIN categories category ON lot.category_id = category.id
+            LEFT JOIN bids bid ON lot.id = bid.lot_id
+WHERE NOW() < lot.closed_at
 GROUP BY lot.id
 ORDER BY lot.created_at DESC;
 # get lot by id with category name
 SET @lot_id = 6;
 SELECT lot.id, lot.name, start_price, image_url, category.name AS category_name
 FROM lots lot
-       JOIN categories category ON lot.category_id = category.id
+            JOIN categories category ON lot.category_id = category.id
 WHERE lot.id = @lot_id
 ORDER BY lot.created_at DESC;
 # update lot name by id
