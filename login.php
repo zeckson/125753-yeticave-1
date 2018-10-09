369e89abc91f3a8f828238c5e8f325045f259acf
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2018-10-09 19:15:20 +0300

    add authentication

--- login.php
+++ login.php
@@ -6,6 +6,31 @@ require_once 'src/user_queries.php';
 $user = [];
 $errors = [];
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $user = $_POST;
+
+    $required = ['email', 'password'];
+    foreach ($required as $key) {
+        $value = $user[$key];
+        if (empty($value)) {
+            $errors[$key] = 'Это поле надо заполнить';
+        }
+    }
+
+    if (!get_user_by_email($connection, $user['email'])) {
+        $errors['email'] = 'Пользователь с таким email не найден';
+    }
+
+    if (empty($errors)) {
+        $logged_in_user = login($connection, $user['email'], $user['password']);
+        if (!$logged_in_user) {
+            $errors['password'] = 'Неверный пароль';
+        } else {
+            header('Location: /index.php');
+            die();
+        }
+    }
+}
 $config = [
     'title' => 'Вход',
     'current_user' => null,
--- src/user_queries.php
+++ src/user_queries.php
@@ -8,12 +8,12 @@ function get_random_user($connection)
 
 function get_user_by_email($connection, $email)
 {
-    return fetch_all($connection, "SELECT id, name FROM users WHERE email=?", [$email]);
+    return fetch_all($connection, "SELECT id FROM users WHERE email=?", [$email]);
 }
 
 function create_new_user($connection, $user)
 {
-    $password = password_hash($user['password'].$user['email'], PASSWORD_BCRYPT);
+    $password = password_hash($user['password'] . $user['email'], PASSWORD_BCRYPT);
 
     $fields = ['name', 'email', 'password', 'info'];
     if ($user['avatar_url']) {
@@ -24,4 +24,31 @@ function create_new_user($connection, $user)
     return insert_into($connection, "INSERT INTO users $query;",
         [$user['name'], $user['email'], $password, $user['info'], $user['avatar_url']]
     );
+}
+
+/**
+ * @param $connection
+ * @param $email
+ * @param $password
+ * @return array|null
+ */
+function login($connection, $email, $password)
+{
+    $user = fetch_all($connection, "SELECT id, name, password, avatar_url FROM users WHERE email=?", [$email]);
+
+    if (!$user) {
+        return null;
+    }
+
+    if (!isset($user[0])) {
+        return null;
+    }
+
+    $user = $user[0];
+
+    if (!password_verify($password . $email, $user['password'])) {
+        return null;
+    }
+
+    return $user;
 }
