2455ca8cac2b489d00d92ab870a3cb86cd0f8326
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2018-10-13 13:16:52 +0300

    handle too big file

--- add.php
+++ add.php
@@ -10,48 +10,53 @@ $errors = [];
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $lot = $_POST;
 
-    $required = ['name', 'description', 'category', 'start_price', 'bid_step', 'closed_at'];
-    foreach ($required as $key) {
-        $value = $lot[$key];
-        if (empty($value)) {
-            $errors[$key] = 'Это поле надо заполнить';
-            continue;
+    $max_size = (int)ini_get('post_max_size') * 1024 * 1024;
+    if (isset($_SERVER["CONTENT_LENGTH"]) && $_SERVER["CONTENT_LENGTH"] > $max_size) {
+        $errors['image'] = "Размер загружаемой картинки превысил $max_size";
+    } else {
+        $required = ['name', 'description', 'category', 'start_price', 'bid_step', 'closed_at'];
+        foreach ($required as $key) {
+            $value = $lot[$key];
+            if (empty($value)) {
+                $errors[$key] = 'Это поле надо заполнить';
+                continue;
+            }
+            switch ($key) {
+                case 'start_price':
+                case 'bid_step':
+                case 'category':
+                    $value = intval($value);
+                    if ($value <= 0) {
+                        $errors[$key] = 'Это поле должно быть больше 0';
+                    } else {
+                        $lot[$key] = $value;
+                    }
+                    break;
+                case 'closed_at':
+                    $close_time = strtotime($value);
+                    $tomorrow = strtotime('tomorrow');
+                    if ($close_time < $tomorrow) {
+                        $errors[$key] = 'Дата закрытия должна быть не раньше чем завтра';
+                    } else {
+                        $lot[$key] = date('Y-m-d H:i:s', $close_time);
+                    }
+                    break;
+            }
         }
-        switch ($key) {
-            case 'start_price':
-            case 'bid_step':
-            case 'category':
-                $value = intval($value);
-                if ($value <= 0) {
-                    $errors[$key] = 'Это поле должно быть больше 0';
-                } else {
-                    $lot[$key] = $value;
-                }
-                break;
-            case 'closed_at':
-                $close_time = strtotime($value);
-                $tomorrow = strtotime('tomorrow');
-                if ($close_time < $tomorrow) {
-                    $errors[$key] = 'Дата закрытия должна быть не раньше чем завтра';
-                } else {
-                    $lot[$key] = date( 'Y-m-d H:i:s', $close_time );
-                }
-                break;
+
+        try {
+            $lot['image'] = get_required_file_name('image');
+        } catch (RuntimeException $e) {
+            $errors['image'] = $e->getMessage();
         }
-    }
 
-    try {
-        $lot['image'] = get_required_file_name('image');
-    } catch (RuntimeException $e) {
-        $errors['image'] = $e->getMessage();
-    }
-
-    if (empty($errors)) {
-        require_once 'src/lot_queries.php';
-        $id = insert_new_lot($connection, $lot, get_session_current_user());
-        require_once 'src/links.php';
-        $link = get_lot_page_link_by_id($id);
-        header("Location: $link"); //
+        if (empty($errors)) {
+            require_once 'src/lot_queries.php';
+            $id = insert_new_lot($connection, $lot, get_session_current_user());
+            require_once 'src/links.php';
+            $link = get_lot_page_link_by_id($id);
+            header("Location: $link"); //
+        }
     }
 }
 $config = [
--- src/common.php
+++ src/common.php
@@ -1,6 +1,9 @@
 <?php
 declare(strict_types=1);
+// Set default timezone to Europe/Moscow
 date_default_timezone_set('Europe/Moscow');
+// Set default upload max size to 5MB in php.ini
+// upload_max_filesize(5 * 1024 * 1024);
 
 require_once 'security.php';
 require_once 'db.php';
