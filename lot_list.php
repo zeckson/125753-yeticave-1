63e88e876875fa2474fea468c3f6ae4dad27161f
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2018-10-14 15:58:25 +0300

    fixes #6

--- lot_list.php
+++ lot_list.php
@@ -4,8 +4,13 @@ require_once 'src/include/common.php';
 require_once 'src/lot_queries.php';
 $lots = get_all_open_lots($connection, $cat_id);
 
+$title = 'Все лоты';
+if ($current_category) {
+    $title .= " в категории «{$current_category['name']}»";
+}
+
 $config = [
-    'title' => 'Заглавная',
+    'title' => $title,
     'content' => include_template('templates/page/list.php', [
         'category' => $current_category,
         'lots' => $lots
