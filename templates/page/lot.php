18533826203d5658926ec947d2ecdf9394679a3a
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2018-10-15 21:28:55 +0300

    fixes https://github.com/zeckson/125753-yeticave-1/issues/12

--- templates/page/lot.php
+++ templates/page/lot.php
@@ -24,36 +24,38 @@ require_once 'src/utils/lot.php';
             <p class="lot-item__category">Категория: <span><?= $lot['category'] ?></span></p>
             <p class="lot-item__description"><?= htmlspecialchars($lot['description']) ?></p>
         </div>
-        <?php if ($user_can_add_bid): ?>
-            <div class="lot-item__right">
-                <div class="lot-item__state">
-                    <div class="lot-item__timer timer"><?= format_period(time_left($lot['closed_at'])) ?></div>
-                    <div class="lot-item__cost-state">
-                        <div class="lot-item__rate">
-                            <span class="lot-item__amount">Текущая цена</span>
-                            <span class="lot-item__cost"><?= format_price($lot['price']) ?></span>
-                        </div>
-                        <div class="lot-item__min-cost">
-                            Мин. ставка <span><?= format_price($lot['price'] + $lot['bid_step']) ?></span>
-                        </div>
+
+        <div class="lot-item__right">
+            <div class="lot-item__state">
+                <div class="lot-item__timer timer"><?= format_period(time_left($lot['closed_at'])) ?></div>
+                <div class="lot-item__cost-state">
+                    <div class="lot-item__rate">
+                        <span class="lot-item__amount">Текущая цена</span>
+                        <span class="lot-item__cost"><?= format_price($lot['price']) ?></span>
                     </div>
-                    <?= include_template("templates/block/add_bid.php", array_merge(['lot' => $lot], $new_bid)) ?>
+                    <div class="lot-item__min-cost">
+                        Мин. ставка <span><?= format_price($lot['price'] + $lot['bid_step']) ?></span>
+                    </div>
                 </div>
-                <?php if (isset($bids)): ?>
-                    <div class="history">
-                        <h3>История ставок (<span><?= sizeof($bids) ?></span>)</h3>
-                        <table class="history__list">
-                            <?php foreach ($bids as $bid): ?>
-                                <tr class="history__item">
-                                    <td class="history__name"><?= htmlspecialchars($bid['name']) ?></td>
-                                    <td class="history__price"><?= format_price($bid['amount']) ?></td>
-                                    <td class="history__time"><?= format_relative_time(strtotime($bid['created_at'])) ?></td>
-                                </tr>
-                            <?php endforeach; ?>
-                        </table>
-                    </div>
+                <?php if ($user_can_add_bid): ?>
+                    <?= include_template("templates/block/add_bid.php", array_merge(['lot' => $lot], $new_bid)) ?>
                 <?php endif; ?>
             </div>
+            <?php if (isset($bids)): ?>
+            <div class="history">
+                <h3>История ставок (<span><?= sizeof($bids) ?></span>)</h3>
+                <table class="history__list">
+                    <?php foreach ($bids as $bid): ?>
+                        <tr class="history__item">
+                            <td class="history__name"><?= htmlspecialchars($bid['name']) ?></td>
+                            <td class="history__price"><?= format_price($bid['amount']) ?></td>
+                            <td class="history__time"><?= format_relative_time(strtotime($bid['created_at'])) ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </table>
+            </div>
+
+        </div>
         <?php endif ?>
     </div>
 </section>
