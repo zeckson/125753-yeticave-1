52b9ba68fa0a10145f1a3e62c40b986eee167472
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2018-10-16 12:21:10 +0300

    fixes https://github.com/zeckson/125753-yeticave-1/issues/19

--- lot.php
+++ lot.php
@@ -4,8 +4,7 @@ require_once 'src/include/common.php';
 $lot_id = intval($_GET['id'] ?? null);
 
 if ($lot_id <= 0) {
-    http_response_code(NOT_FOUND_HTTP_STATUS_CODE);
-    die();
+    show_not_found_page($navigation);
 }
 
 require_once 'src/lot_queries.php';
@@ -13,8 +12,7 @@ $lot = get_lot_by_id($connection, $lot_id);
 
 require_once 'src/utils/lot.php';
 if (!$lot) {
-    http_response_code(NOT_FOUND_HTTP_STATUS_CODE);
-    die();
+    show_not_found_page($navigation);
 }
 
 render_lot_page($connection, $navigation, $lot);
---
+++ templates/page/404.php
@@ -0,0 +1,10 @@
+<?php
+?>
+<div class="container">
+    <section class="lots">
+        <h1>Запрашиваемая страница не найдена</h1>
+    </section>
+</div>
+
+
+  
